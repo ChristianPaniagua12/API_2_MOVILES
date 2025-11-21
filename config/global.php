a1281d9d87529a8c375636d1feb9108e3a1be603
CHRISTIAN_PC_12 <crisp314528@gmail.com>
2025-11-21 00:34:42 -0600

    ajuste en credenciales de bd

--- config/global.php
+++ config/global.php
@@ -1,19 +1,21 @@
 <?php
-$databaseUrl = getenv("MYSQL_URL") ?: getenv("MYSQL_PUBLIC_URL");
+
+$databaseUrl = getenv("MYSQL_URL"); 
 
 if ($databaseUrl) {
     $parsedUrl = parse_url($databaseUrl);
+
     define("DB_HOST", $parsedUrl["host"]);
     define("DB_PORT", $parsedUrl["port"]);
     define("DB_NAME", ltrim($parsedUrl["path"], "/"));
     define("DB_USERNAME", $parsedUrl["user"]);
-    define("DB_PASSWORD", $parsedUrl["pass"] ?: "KYNhVYJskNDfOQXCYtlfgMbtEmxIVRvn");
+    define("DB_PASSWORD", $parsedUrl["pass"]);
 } else {
-    define("DB_HOST", getenv("MYSQLHOST") ?: "mysql.railway.internal");
-    define("DB_PORT", getenv("MYSQLPORT") ?: "3306");
-    define("DB_NAME", getenv("MYSQLDATABASE") ?: "proyecto2web");
-    define("DB_USERNAME", getenv("MYSQLUSER") ?: "root");
-    define("DB_PASSWORD", getenv("MYSQLPASSWORD") ?: getenv("MYSQL_ROOT_PASSWORD") ?: "KYNhVYJskNDfOQXCYtlfgMbtEmxIVRvn");
+    define("DB_HOST", getenv("MYSQLHOST"));
+    define("DB_PORT", getenv("MYSQLPORT"));
+    define("DB_NAME", getenv("MYSQLDATABASE"));
+    define("DB_USERNAME", getenv("MYSQLUSER"));
+    define("DB_PASSWORD", getenv("MYSQLPASSWORD"));
 }
 
 define("DB_ENCODE", "utf8mb4");
